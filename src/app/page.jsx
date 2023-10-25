b071fcd1d0160a8040fdb8f1ed6192bf693f0024
Surya <surya.vallamkonda@gmail.com>
2023-10-25 13:55:40 -0400

    u

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,7 +14,7 @@ function Home() {
       <div className="grid h-full grid-cols-12 grid-rows-1">
         <div className="col-start-2 col-span-4 h-full self-center ml-0 p-10 bg-inherit">
           <h1 className="mt-[40vh] w-fit mb-4 text-7xl font-extrabold tracking-tight leading-none p-5 text-white drop-shadow-lg">
-          Design 3D Rooms Instantly
+            Design 3D Rooms Instantly
           </h1>
           <p className="text-neutral-300 w-fit drop-shadow-lg p-5">
             No hassle, just drag and drop.
@@ -25,46 +25,46 @@ function Home() {
             rounded-lg bg-slate-950 pointer-events-auto shadow-lg gap-3"
           >
             Get started
-            <BiArrowToRight size="32"/>
+            <BiArrowToRight size="32" />
           </a>
         </div>
         <div className="col-start-7 col-span-5 h-full flex flex-col justify-center">
           <div className="h-2/3 border-slate-600 rounded-md border-[1rem] pointer-events-none">
-          <Canvas
-        gl={{ logarithmicDepthBuffer: true }}
-        shadows
-        camera={{ position: [0, 3, 0], fov: 40 }}
-      >
-        <fog attach="fog" args={["black", 15, 21.5]} />
-        <Stage
-          intensity={0.5}
-          environment="city"
-          shadows={{ type: "accumulative", bias: -0.001 }}
-          adjustCamera={false}
-        >
-          <Room rotation={[0, Math.PI, 0]} position={[0, -1, 0]} />
-        </Stage>
-        <Grid
-          renderOrder={-1}
-          position={[0, -1.85, 0]}
-          infiniteGrid
-          cellSize={0.6}
-          cellThickness={0.6}
-          sectionSize={3.3}
-          sectionThickness={1.5}
-          sectionColor={[0.5, 0.5, 10]}
-          fadeDistance={30}
-        />
-        <OrbitControls
-          autoRotate
-          autoRotateSpeed={0.05}
-          enableZoom={false}
-          makeDefault
-          minPolarAngle={Math.PI / 2}
-          maxPolarAngle={Math.PI / 2}
-        />
-        <Environment background preset="sunset" blur={0.8} />
-      </Canvas>
+            <Canvas
+              gl={{ logarithmicDepthBuffer: true }}
+              shadows
+              camera={{ position: [0, 3, 0], fov: 40 }}
+            >
+              <fog attach="fog" args={["black", 15, 21.5]} />
+              <Stage
+                intensity={0.5}
+                environment="city"
+                shadows={{ type: "accumulative", bias: -0.001 }}
+                adjustCamera={false}
+              >
+                <Room rotation={[0, Math.PI, 0]} position={[0, -1, 0]} />
+              </Stage>
+              <Grid
+                renderOrder={-1}
+                position={[0, -1.85, 0]}
+                infiniteGrid
+                cellSize={0.6}
+                cellThickness={0.6}
+                sectionSize={3.3}
+                sectionThickness={1.5}
+                sectionColor={[0.5, 0.5, 10]}
+                fadeDistance={30}
+              />
+              <OrbitControls
+                autoRotate
+                autoRotateSpeed={0.05}
+                enableZoom={false}
+                makeDefault
+                minPolarAngle={Math.PI / 2}
+                maxPolarAngle={Math.PI / 2}
+              />
+              {/* <Environment background preset="sunset" blur={0.8} /> */}
+            </Canvas>
             {/* <img src="demo_room.png" className="w-full h-full opacity-70"/> */}
           </div>
         </div>
--- src/app/planner/page.jsx
+++ src/app/planner/page.jsx
@@ -23,20 +23,23 @@ import { OrbitControls, Stage, Grid, Environment } from "@react-three/drei";
 
 // save, import, undo, redo, select, delete, menu
 function Toolbar() {
+  let size1 = "28";
   return (
-    <section className="bg-slate-100 h-screen basis-[6%] relative flex flex-col text-white shadow-lg items-center">
-      <ToolbarComponent icon={<BiPointer size="32" />} />
-      <ToolbarComponent icon={<BiSave size="32" />} />
-      <ToolbarComponent icon={<BiFolderOpen size="32" />} />
-      <ToolbarComponent icon={<BiDownload size="32" />} />
-      <ToolbarComponent icon={<BiRedo size="32" />} />
-      <ToolbarComponent icon={<BiUndo size="32" />} />
-      <ToolbarComponent icon={<BiTrash size="32" />} />
-      <ToolbarComponent icon={<BiPlus size="32" />} />
-      <i className="bg-slate-900 h-1 w-11/12" />
+    <section className="bg-slate-100 h-full basis-[6%] relative flex flex-col text-white shadow-lg items-center align-middle justify-between">
+      <ToolbarComponent icon={<BiPointer size={size1} />} />
+      <ToolbarComponent icon={<BiSave size={size1} />} />
+      <ToolbarComponent icon={<BiFolderOpen size={size1} />} />
+      <ToolbarComponent icon={<BiDownload size={size1} />} />
+      <ToolbarComponent icon={<BiRedo size={size1} />} />
+      <ToolbarComponent icon={<BiUndo size={size1} />} />
+      <ToolbarComponent icon={<BiTrash size={size1} />} />
+      <i className="bg-inherit h-10 w-11/12" />
 
-      <ToolbarComponent icon={<BiCamera size="32" />} />
-      <ToolbarComponent icon={<BiShare size="32" />} />
+      <ToolbarComponent icon={<BiPlus size={size1} />} />
+      <ToolbarComponent icon={<BiCamera size={size1} />} />
+      <ToolbarComponent icon={<BiShare size={size1} />} />
+
+      <i className="bg-black h-20 w-11/12 opacity-0" />
     </section>
   );
 }
@@ -55,8 +58,8 @@ function ToolbarComponent({ icon }) {
 function Planner() {
   return (
     <div className="w-full">
-      {/* <h1 className="m-[33%] text-3xl">def a canvas so fr</h1> */}
-      <Canvas
+      <h1 className="m-[33%] text-3xl">def a canvas so fr</h1>
+      {/* <Canvas
         gl={{ logarithmicDepthBuffer: true }}
         shadows
         camera={{ position: [-15, 0, 10], fov: 25 }}
@@ -89,15 +92,15 @@ function Planner() {
           minPolarAngle={Math.PI / 2}
           maxPolarAngle={Math.PI / 2}
         />
-        <Environment background preset="sunset" blur={0.8} />
-      </Canvas>
+      </Canvas> */}
+      {/* <Environment background preset="sunset" blur={0.8} /> */}
     </div>
   );
 }
 
 function Catalog() {
   return (
-    <section className="bg-slate-100 basis-1/6 overflow-y-scroll flex flex-col text-5xl items-center font-extrabold gap-4">
+    <section className="bg-slate-100 basis-1/6 overflow-y-scroll flex flex-col text-5xl items-center font-bold gap-4">
       <h1 className="sticky shadow-lg shadow-neutral-600 rounded-b-xl top-0 z-[1] bg-slate-400 p-5 w-full text-center">
         CATALOG
       </h1>
@@ -123,7 +126,7 @@ function Catalog() {
       />
       <CatalogProduct
         img={<img src="icons/blank.png" className="rounded-2xl opacity-0" />}
-        name={"Armchair"}
+        name={""}
       />
     </section>
   );
@@ -133,7 +136,7 @@ function CatalogProduct({ img, name }) {
   return (
     <div className="relative flex flex-col items-center justify-center w-2/3 mx-auto p-2">
       {img}
-      <h2 className="text-xl text-slate-700 font-bold font-serif">{name}</h2>
+      <h2 className="text-xl text-slate-700 font-serif">{name}</h2>
     </div>
   );
 }
