65290d13574ec77db25a966aebb90f258f87d709
suryavallamkonda <surya.vallamkonda@gmail.com>
2023-12-12 03:35:01 -0500

    idk bruh

--- src/app/planner/page.jsx
+++ src/app/planner/page.jsx
@@ -1,16 +1,12 @@
 "use client";
-
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../globals.css";
-
 import {
   BiSave,
   BiFolderOpen,
-  BiMenu,
   BiRedo,
   BiUndo,
   BiTrash,
-  BiArrowBack,
   BiPlus,
   BiDownload,
   BiCamera,
@@ -20,7 +16,6 @@ import {
 import { Canvas } from "@react-three/fiber";
 import { Armchair } from "@/catalog/Armchair";
 import { OrbitControls, Stage, Grid, Environment } from "@react-three/drei";
-
 // save, import, undo, redo, select, delete, menu
 function Toolbar() {
   return (
@@ -40,7 +35,6 @@ function Toolbar() {
     </section>
   );
 }
-
 function ToolbarComponent({ icon }) {
   return (
     <div
@@ -51,12 +45,38 @@ function ToolbarComponent({ icon }) {
     </div>
   );
 }
-
 function Planner() {
+  const [models, setModels] = useState([]);
+  const [selectedModel, setSelectedModel] = useState(null);
+
+  const handleAddModel = (model, event) => {
+    const { clientX, clientY } = event.nativeEvent;
+    const ray = new THREE.Raycaster();
+    ray.setFromCamera({ x: clientX, y: clientY }, camera);
+    const intersects = ray.intersectObjects(scene.children);
+
+    if (intersects.length > 0) {
+      const position = intersects[0].point;
+      const gridX = Math.round(position.x / gridSize);
+      const gridY = Math.round(position.y / gridSize);
+      const gridZ = Math.round(position.z / gridSize);
+      const closestGridPoint = new THREE.Vector3(
+        gridX * gridSize,
+        gridY * gridSize,
+        gridZ * gridSize
+      );
+
+      const newModel = model({ position: closestGridPoint });
+      setModels([...models, newModel]);
+      scene.add(newModel.mesh);
+    } else {
+      console.warn("Model outside grid");
+    }
+  };
   return (
     <div className="w-full">
-      {/* <h1 className="m-[33%] text-3xl">def a canvas so fr</h1> */}
       <Canvas
+        onClick={(event) => handleAddModel(selectedModel, event)}
         gl={{ logarithmicDepthBuffer: true }}
         shadows
         camera={{ position: [-15, 0, 10], fov: 25 }}
@@ -68,9 +88,54 @@ function Planner() {
           shadows={{ type: "accumulative", bias: -0.001 }}
           adjustCamera={false}
         >
-          <Armchair rotation={[0, Math.PI, 0]} />
+          {models.map((model) => model)}
         </Stage>
-        <Grid
+        
+      </Canvas>
+    </div>
+  );
+}
+function Catalog({ onAddModel }) {
+  return (
+    <section className="bg-slate-100 basis-1/6 overflow-y-scroll flex flex-col text-5xl items-center font-extrabold gap-4">
+      <h1 className="sticky shadow-lg shadow-neutral-600 rounded-b-xl top-0 z-[1] bg-slate-400 p-5 w-full text-center">
+        CATALOG
+      </h1>
+      <CatalogProduct
+        img={<img src="icons/armchair-icon.jpg" className="rounded-2xl" />}
+        name="Armchair"
+        onAddModel={onAddModel}
+        model={Armchair}
+      />
+    </section>
+  );
+}
+function CatalogProduct({ img, name, onAddModel, model }) {
+  return (
+    <div className="relative flex flex-col items-center justify-center w-2/3 mx-auto p-2">
+      {img}
+      <h2 className="text-xl text-slate-700 font-bold font-serif">{name}</h2>
+      <button onClick={() => onAddModel(model)}>Add Model</button>
+    </div>
+  );
+}
+const models = [Armchair];
+export default function App() {
+  return (
+    <section className="relative flex flex-row h-screen">
+      <Toolbar />
+      <Planner />
+      <Catalog />
+    </section>
+  );
+}
+
+
+
+
+
+
+<Grid
           renderOrder={-1}
           position={[0, -1.85, 0]}
           infiniteGrid
@@ -90,60 +155,3 @@ function Planner() {
           maxPolarAngle={Math.PI / 2}
         />
         <Environment background preset="sunset" blur={0.8} />
-      </Canvas>
-    </div>
-  );
-}
-
-function Catalog() {
-  return (
-    <section className="bg-slate-100 basis-1/6 overflow-y-scroll flex flex-col text-5xl items-center font-extrabold gap-4">
-      <h1 className="sticky shadow-lg shadow-neutral-600 rounded-b-xl top-0 z-[1] bg-slate-400 p-5 w-full text-center">
-        CATALOG
-      </h1>
-      <CatalogProduct
-        img={<img src="icons/armchair-icon.jpg" className="rounded-2xl" />}
-        name={"Armchair"}
-      />
-      <CatalogProduct
-        img={<img src="icons/armchair-icon.jpg" className="rounded-2xl" />}
-        name={"Armchair"}
-      />
-      <CatalogProduct
-        img={<img src="icons/armchair-icon.jpg" className="rounded-2xl" />}
-        name={"Armchair"}
-      />
-      <CatalogProduct
-        img={<img src="icons/armchair-icon.jpg" className="rounded-2xl" />}
-        name={"Armchair"}
-      />
-      <CatalogProduct
-        img={<img src="icons/armchair-icon.jpg" className="rounded-2xl" />}
-        name={"Armchair"}
-      />
-      <CatalogProduct
-        img={<img src="icons/blank.png" className="rounded-2xl opacity-0" />}
-        name={"Armchair"}
-      />
-    </section>
-  );
-}
-
-function CatalogProduct({ img, name }) {
-  return (
-    <div className="relative flex flex-col items-center justify-center w-2/3 mx-auto p-2">
-      {img}
-      <h2 className="text-xl text-slate-700 font-bold font-serif">{name}</h2>
-    </div>
-  );
-}
-
-export default function App() {
-  return (
-    <section className="relative flex flex-row h-screen">
-      <Toolbar />
-      <Planner />
-      <Catalog />
-    </section>
-  );
-}
